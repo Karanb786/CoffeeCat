b1a98e5593375c05e3db0401bbea01ca13069c14
Karanb786 <karanbagle786@icloud.com>
2018-06-17 15:52:49 +0530

    Email Ordering
    Order is placed using email

--- app/src/main/java/com/coffee/coffeecat/MainActivity.java
+++ app/src/main/java/com/coffee/coffeecat/MainActivity.java
@@ -1,18 +1,22 @@
 package com.coffee.coffeecat;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
     int noofcoffee=2;
+    String email = "karanbagle420@gmail.com";
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +37,36 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chocoBox=findViewById(R.id.chocolate);
         boolean haschoco=chocoBox.isChecked();
 
-        int price = calculatePrice();
+        int price = calculatePrice(hascheamBox,haschoco);
         String priceMessage =createOrderSummery(name, price, hascheamBox,haschoco);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for "+name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
         displayMessage(priceMessage);
+
+
+
 
 
     }
 
-    private int calculatePrice(){return noofcoffee*5; }
+    private int calculatePrice(boolean addCream , boolean addChoo) {
+        int basePrice = 5;
+         if(addCream){
+             basePrice = basePrice+1;
+         }
+         if(addChoo){
+             basePrice=basePrice+2;
+         }
+        return noofcoffee* basePrice;
+    }
 
     private String createOrderSummery(String name, int price, boolean hascheamBox,boolean haschoco){
         String priceMessage = "Name : "+name;
@@ -53,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 public void increment(View view){
+        if(noofcoffee==10){
+            Toast.makeText(this,"You canot order more then 10 coffee",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         noofcoffee = noofcoffee+1;
         displayquintity(noofcoffee);
 
@@ -60,6 +91,11 @@ public void increment(View view){
 
 
 public void decrement(View view){
+        if(noofcoffee==1){
+            Toast.makeText(this,"You must order more then 1 coffee",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
 
     noofcoffee = noofcoffee-1;
 
