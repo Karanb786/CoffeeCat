8b2dd48b9eb5103a1c086f2816b57153e872e0ef
Karanb786 <karanbagle786@icloud.com>
2018-06-17 14:29:46 +0530

    Name Field and CheckBox
    You can add Name and Checkbox

--- app/src/main/java/com/coffee/coffeecat/MainActivity.java
+++ app/src/main/java/com/coffee/coffeecat/MainActivity.java
@@ -1,22 +1,20 @@
 package com.coffee.coffeecat;
 
 
-
-
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
     int noofcoffee=2;
-    @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -26,33 +24,52 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = noofcoffee*5;
-        String priceMessage ="Price: $" +price;
-        priceMessage=priceMessage+ "\nThank You!";
+        EditText nameField = findViewById(R.id.userName);
+        String name = nameField.getText().toString();
+
+        CheckBox cramBox =findViewById(R.id.cream);
+        boolean hascheamBox= cramBox.isChecked();
+
+        CheckBox chocoBox=findViewById(R.id.chocolate);
+        boolean haschoco=chocoBox.isChecked();
+
+        int price = calculatePrice();
+        String priceMessage =createOrderSummery(name, price, hascheamBox,haschoco);
         displayMessage(priceMessage);
 
 
     }
+
+    private int calculatePrice(){return noofcoffee*5; }
+
+    private String createOrderSummery(String name, int price, boolean hascheamBox,boolean haschoco){
+        String priceMessage = "Name : "+name;
+        priceMessage +="\nAdd wwhipped cream? " +hascheamBox;
+        priceMessage +="\nAdd Chocolate? " +haschoco;
+        priceMessage += "\nQuintity :"+noofcoffee;
+        priceMessage +="\nPrice: $" +price;
+        priceMessage+= "\nThank You!";
+        return priceMessage;
+
+    }
 public void increment(View view){
         noofcoffee = noofcoffee+1;
-        display(noofcoffee);
+        displayquintity(noofcoffee);
 
 }
+
 
 public void decrement(View view){
 
     noofcoffee = noofcoffee-1;
 
-    display(noofcoffee);
+    displayquintity(noofcoffee);
 
 }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
+
 
 
 
@@ -60,16 +77,22 @@ public void decrement(View view){
      * This method displays the given price on the screen.
      */
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
 
     /**
      * This method displays the given text on the screen.
+     *
      */
+
+
+    private void displayquintity(int numberOFCoffee){
+    TextView quintityText= findViewById(R.id.quantity_text_view);
+    quintityText.setText(""+numberOFCoffee);
+
+    }
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummeryTextViw = (TextView) findViewById(R.id.order_summery_text_view);
+
+        orderSummeryTextViw.setText(message);
     }
 }
